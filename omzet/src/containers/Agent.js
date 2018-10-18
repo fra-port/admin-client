49d438ac30a854415c66cd2661d8b97547fd759a
sumarsanaadi <sumarsana.adi@gmail.com>
2018-10-18 10:14:38 +0700

    react native init

---
+++ omzet/src/containers/Agent.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+class Agent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Product</Text>
+      </View>
+    )
+  }
+}
+
+
+export default (Agent)
