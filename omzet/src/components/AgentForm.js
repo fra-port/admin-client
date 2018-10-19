a522460d97a712448bd29c184775775256915ec2
sumarsanaadi <sumarsana.adi@gmail.com>
2018-10-19 16:42:10 +0700

    upload pic

--- omzet/src/components/AgentForm.js
+++ omzet/src/components/AgentForm.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Thumbnail, Button, Form, Item, Input, Label  } from 'native-base';
-import { Text, View, StyleSheet, ScrollView } from 'react-native'
+import { Thumbnail, Button, Form, Item, Input  } from 'native-base';
+import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+import axios from 'axios'
+const serverURL = "http://35.240.197.42"
 
 class AgentForm extends Component {
   constructor(props) {
@@ -12,7 +15,13 @@ class AgentForm extends Component {
       email : '',
       address : '',
       phoneNumber : '',
-      formType : "Add"
+      propicURL: 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png',
+      formType : "Add",
+      srcImg: '',
+      fileName: '',
+      type: '',
+      uri: '',
+      loading: false,
     }
   }
 
@@ -25,16 +34,97 @@ class AgentForm extends Component {
         email : this.props.data.email,
         address : this.props.data.address,
         phoneNumber : this.props.data.phoneNumber,
+        propicURL: this.props.data.propicURL,
         formType : "Edit"
       })
     }
   }
 
+  selectImg = () => {
+    const options = {
+      title: 'Options',
+      takePhotoButtonTitle: 'Camera',
+      chooseFromLibraryButtonTitle: 'Gallery',
+    }    
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        console.log("User cancelled image picker")
+      } else if (response.error) {
+        console.log('Image picker error: ', response.error)
+      } else {
+        let source = { uri: response.uri }
+        console.log(response.fileName)
+        this.setState({
+          srcImg: source,
+          // propicURL: source,
+          filename: response.fileName,
+          type: response.type,
+          uri: response.uri
+        })
+      }
+    })
+  }
+
+  uploadImage = async () => {
+    try {
+      let formData = new FormData()
+      let type = this.state.type
+      formData.append('image', { uri: this.state.uri, name: this.state.filename, type })
+      axios.post(`${serverURL}/image`, formData , {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+          }
+      })
+      .then (({data})=> {
+        this.setState({
+          propicURL: data.imageURL
+        })
+      })
+      .catch (err => {
+        Alert.alert(
+          'Alert',
+          `${err.message}`,
+          [
+            {text: 'OK'},
+          ],
+          { cancelable: false }
+        )
+      }) 
+    } catch(err) {
+      Alert.alert(
+        'Alert',
+        `${err.message}`,
+        [
+          {text: 'OK'},
+        ],
+        { cancelable: false }
+      )
+    }
+  }
+
+  handleClick = async () => {
+    await this.uploadImage()
+    this.props.formMethod({
+      firstName : this.state.firstName,
+      lastName : this.state.lastName,
+      idTelegram : this.state.idTelegram,
+      email : this.state.email,
+      address : this.state.address,
+      phoneNumber : this.state.phoneNumber,
+      propicURL : this.state.propicURL
+    })
+  }
+
   render() {
     return (
       <ScrollView>
-        <View style={{alignItems: 'center'}}>
-          <Thumbnail large source={{ uri: 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/male3-512.png' }} />
+        <View style={{alignItems: 'center', border: '5 solid'}}>
+          <TouchableOpacity
+            onPress={this.selectImg}
+          >
+            <Thumbnail large source={{ uri: this.state.propicURL }} />
+          </TouchableOpacity>
+          <Text style={{fontSize: 10}}>click image to change propic</Text>
         </View>
         <View style={{marginTop: 10, marginHorizontal: 20}}>
           <Form>
@@ -97,14 +187,7 @@ class AgentForm extends Component {
         <View style={styles.row}>
           <Button 
             success
-            onPress={() => this.props.formMethod({
-              firstName : this.state.firstName,
-              lastName : this.state.lastName,
-              idTelegram : this.state.idTelegram,
-              email : this.state.email,
-              address : this.state.address,
-              phoneNumber : this.state.phoneNumber,
-            })}
+            onPress={() => this.handleClick()}
             style={{ width: 80, justifyContent: "center"}}
           >
               <Text style={{ textAlign: 'center', color:'white'}}>{this.state.formType}</Text>
