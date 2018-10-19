d1db6707504119ed7428d324d7e58867857d0e6c
HelmiYogantara <helmiyogantara@gmail.com>
2018-10-19 22:25:34 +0700

    pie

--- omzet/android/app/src/main/java/com/omzet/MainApplication.java
+++ omzet/android/app/src/main/java/com/omzet/MainApplication.java
@@ -30,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ImagePickerPackage(),
+            // new SvgPackage(),
             new RNFirebasePackage(),
             new VectorIconsPackage(),
             new RNFirebaseAuthPackage()
--- omzet/src/components/reportThumbnail.js
+++ omzet/src/components/reportThumbnail.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
-import { View, FlatList } from 'react-native'
+import { View, FlatList, StatusBar, StyleSheet } from 'react-native'
 import { List, ListItem, Thumbnail, Card, CardItem, Body, Text, Left, Right, Button } from "native-base";
+import PieChart from 'react-native-pie-chart';
 
 export default class ReportThumbnail extends Component {
 
     render() {
         const detail = this.props.detail
-        
+        const chart_wh = 250
+        const series = [123, 321, 123, 789, 537]
+        const sliceColor = ['#F44336', '#2196F3', '#FFEB3B', '#4CAF50', '#FF9800']
         return (
             <View>
                 <Card>
@@ -30,6 +33,7 @@ export default class ReportThumbnail extends Component {
                                         ({ item, index }) => (
                                             <Text style={{ justifyContent: 'flex-end' }}>
                                                 {item.name}  = {item.totalSelling} pcs
+                                                {/* {JSON.stringify(item)} */}
                                             </Text>
                                         )
                                     }
@@ -38,7 +42,26 @@ export default class ReportThumbnail extends Component {
                         </Body>
                     </CardItem>
                 </Card>
-
+                <View style={styles.container}>
+                    <StatusBar
+                        hidden={true}
+                    />
+                    <Text style={styles.title}>Basic</Text>
+                    <PieChart
+                        chart_wh={chart_wh}
+                        series={series}
+                        sliceColor={sliceColor}
+                    />
+                    <Text style={styles.title}>Doughnut</Text>
+                    <PieChart
+                        chart_wh={chart_wh}
+                        series={series}
+                        sliceColor={sliceColor}
+                        doughnut={true}
+                        coverRadius={0.45}
+                        coverFill={'#FFF'}
+                    />
+                </View>
                 <List
                     dataArray={detail.result}
                     renderRow={(item) =>
@@ -68,3 +91,14 @@ export default class ReportThumbnail extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center'
+    },
+    title: {
+      fontSize: 24,
+      margin: 10
+    }
+  });
